bff5a390e48ddb812e0745e27d755120a47e901f
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2023-10-12 13:54:00 +0300

    fix: lint and types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { registerPlugin } from '@playkit-js/kaltura-player-js';
 import { pluginName, SlatePlugin } from './slate-plugin';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 registerPlugin(pluginName, SlatePlugin);
